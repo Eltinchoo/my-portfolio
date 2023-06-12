19b6e29e9841081ed57ef22700fe84cdb6aeb9f0
eltinchoo <mattpaganini07@hotmail.com>
2023-06-12 14:34:56 -0400

    responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer'>
-            <h5 className='footer-text'>Martin Paredes - Web Developer © 2023 All rights Reserved
+            <h5 className='footer-text'>Martin Paredes - Web Developer © 2023 All Rights Reserved
             </h5>
         </div>
     );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="presentation">
       <div className="martin-container">
         <div className="tech-container">
-          <h1 className="name">Hi! I'm Martin "Tincho"</h1>
+          <h1 className="name">Hi! I'm Martin</h1>
           <h2 className="occupation">Front-end Web Developer</h2>
         </div>
         <div className="circle-img">
@@ -14,7 +14,7 @@ const Home = () => {
         </div>
         <p className="text-presentation">
           I love developing web applications and I love solving problems with a
-          computer and a cup of coffee. I am continually learning new technology
+          computer and a cup of coffee. I'm continually learning new technology
           and applying my new skills. It will be a pleasure to work together. "
         </p>
         <a className="download-cv-btn" href="./cv.pdf" download>
